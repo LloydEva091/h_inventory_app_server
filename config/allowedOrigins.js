9ceebffc49d3fffaf0f8f1cded136d74d0d96f17
Lloyd Evangelista <lloyd_eva@hotmail.com>
2023-03-31 16:44:41 +0100

    Added allowed origins url for aws

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
-    'http://localhost:3000',
-    'https://lloyd-proj-hons.duckdns.org'
+    // 'http://localhost:3000',
+    'https://lloyd-hons-proj.duckdns.org',
+    'https://www.lloyd-hons-proj.duckdns.org'
 ]
 
 module.exports = allowedOrigins
